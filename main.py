549923d0b86c9ee6b6e20ab52e5ec077e7102df7
hammad azhar <mhammadazhar28@gmail.com>
2022-03-15 18:22:49 +0500

    some changing in the code of todo add files in the code

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 # In this program user give the file name and data to store!
 
-fileName1 = input("1st file name plz: ")
-text = input("Add data in file: ")
-f = open(fileName1+'.file', "w")
-f.write(text)
-f.close()
+total_title = 3
 
-f = open("all file name.txt", 'a')
-f.write(fileName1)
-f.close()
+for i in range(total_title):
 
-fileName2 = input("2nd file name plz: ")
-text = input("Add data in file: ")
-f = open(fileName2+'.file', "w")
-f.write(text)
-f.close()
+    fileName = input(f"{i+1} file name plz: ")
+    text = input("Add data in file: ")
+    total_title = fileName
 
-f = open("all file name.txt", 'a')
-f.write("\n"+fileName2)
-f.close()
+    f = open(fileName+'.file', "w")
+    f.write(text)
+    f.close()
 
-fileName3 = input("3rd file name plz: ")
-text = input("Add data in file: ")
-f = open(fileName3+'.file', "w")
-f.write(text)
-f.close()
-
-f = open("all file name.txt", 'a')
-f.write("\n"+fileName3)
-f.close()
+    f = open("all_file_name.txt", "a")
+    f.write(fileName+",")
+    f.close()
 
 print("\nThe given content is written on the file successfully!")
